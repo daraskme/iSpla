170c269bca4f8ea72b17e42998f87c3aae1194e4
Hiroshi Inoue <102633287+daraskme@users.noreply.github.com>
2023-10-06 01:08:54 +0900

    Initial Commit

---
+++ iSpla/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iSpla
+//
+//  Created by hi on 2023/10/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iSpla/iSplaApp.swift
@@ -0,0 +1,17 @@
+//
+//  iSplaApp.swift
+//  iSpla
+//
+//  Created by hi on 2023/10/06.
+//
+
+import SwiftUI
+
+@main
+struct iSplaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
